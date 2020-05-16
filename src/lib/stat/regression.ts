356a98144d16e5b36b11db67a21f9f8447e04aaa
Johan <johan@macair2020>
2020-05-16 21:56:31 +0200

    added class for regression

--- src/lib/stat/regression.ts
+++ src/lib/stat/regression.ts
@@ -1,12 +1,54 @@
+// taken from https://bitbucket.org/jboissard/ch.nexys.math/src/master/src/main/scala/ch/nexys/regression/Linear.scala
 // http://www.maths.qmul.ac.uk/~bb/SM_I_2013_LecturesWeek_6.pdf
 import * as M from '../matrix'
 import * as T from '../type';
 
-export const regression = (x: T.Matrix, py: T.Vector) => {
-  const y = M.transpose([py])
-  const xt = M.transpose(x)
-  const z = M.inverse(M.multiplication(xt, x))
-  const preb = M.multiplication(xt, y)
+/*
+	finds best coefficient according to least square for liner regression
 
-  return M.multiplication(z, preb)
+	solves:
+			y		=  X 		a
+			[n x 1]	= [n x m] [m x 1]
+	
+	Constructor #1: for y = mx +q (only one dimension)
+	@params x_in: horizontal vector
+	@params y_in: vertical vector
+
+	Constructor #2: general case
+	@params x: input matrix (with dummy column of ones, if wished)
+	@params y: output
+*/
+export default class Linear{
+  x:T.Matrix;
+  y:T.Matrix;
+  g:T.Matrix;
+  z:T.Matrix;
+  a:T.Matrix;
+
+
+  constructor(x: T.Matrix, y: T.Matrix | T.Vector) {
+    if(Array.isArray(y[0])) {
+      this.x = x;
+      this.y = y as T.Matrix;
+    } else {
+      this.x = x;
+      this.y = M.transpose([y as T.Vector]);
+    }
+
+    const xt = M.transpose(this.x)
+    // Grammian Matrix - http://en.wikipedia.org/wiki/Gramian_matrix
+    this.g = M.multiplication(xt, this.x);
+    this.z = M.inverse(this.g);
+    const preb = M.multiplication(xt, this.y);
+    this.a = M.multiplication(this.z, preb);
+  }
+
+  estimateY(x = this.x):T.Matrix {
+    return M.multiplication(x, this.a)
+  }
+}
+
+export const regression = (x: T.Matrix, py: T.Vector) => {
+  const L = new Linear(x, py);
+  return L.a;
 }
