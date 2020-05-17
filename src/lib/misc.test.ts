4a6c7a5649834dc065130bb0d83a290327cccbb4
Johan <johan@macair2020>
2020-05-17 16:41:07 +0200

    fibo and golden ration

--- src/lib/misc.test.ts
+++ src/lib/misc.test.ts
@@ -1,5 +1,15 @@
 import * as M from './misc';
+import * as S from './series';
 
 test('golden ration', () => {
   expect(M.goldenRatio).toBeCloseTo(1.618)
+})
+
+test('golden ratio and fibonacci', () => {
+  // two consecutive values from the fibo series ratio tend to golden ratio value
+  const n = 30
+  const fs = S.fibonacci(n);
+  
+  expect(fs[n]/fs[n-1]).toBeCloseTo(M.goldenRatio, 3)
+
 })
--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,1 +1,4 @@
+/**
+ * https://en.wikipedia.org/wiki/Golden_ratio
+ */
 export const goldenRatio = (1 + Math.sqrt(5))/2;
