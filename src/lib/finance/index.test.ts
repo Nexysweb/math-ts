ff7e508c978785f3b1fe1017335aa75e1d02d845
Johan <johan@macair2020>
2020-05-22 12:10:45 +0200

    publish pages

--- src/lib/finance/index.test.ts
+++ src/lib/finance/index.test.ts
@@ -2,4 +2,6 @@ import * as I from './index';
 
 test('import', () => {
   expect(typeof I.Loan).toEqual('object')
+  expect(typeof I.Bond).toEqual('object')
+  expect(typeof I.Option).toEqual('object')
 })
