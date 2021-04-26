c831afec0e14fd695b7a0c05886cc30c44916fad
johnb8005 <johnb8005@users.noreply.github.com>
2021-04-26 10:59:31 +0000

    deploy: 72d4b3e9984738e68bd170c102eca97ae631f0c5

---
+++ precache-manifest.3ae96944fc8189855b185dd710cfa163.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c658f7bd8473efe8d76f6658a93db9e5",
+    "url": "/math-ts/index.html"
+  },
+  {
+    "revision": "c317ae56aab6fa5d3d78",
+    "url": "/math-ts/static/js/2.6554adf1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/math-ts/static/js/2.6554adf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bebe8929d786a685ec",
+    "url": "/math-ts/static/js/main.319bd3f8.chunk.js"
+  },
+  {
+    "revision": "5eac1f1236133c937e7b",
+    "url": "/math-ts/static/js/runtime-main.cce5675a.js"
+  }
+]);
